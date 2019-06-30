8b8136ac797cdc4f3ce78a8264b227392f2c56de
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-06-30 20:51:41 +0200

    try save information

--- src/App.js
+++ src/App.js
@@ -2,54 +2,91 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [],
+      date: '',
+      mood: '',
+      message: ''
+    }
+    this.handleDate = this.handleDate.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
+    this.handleMood = this.handleMood.bind(this);
+    this.saveData = this.saveData.bind(this);
+  }
+
+  handleDate(event){
+    this.setState({
+      date: event.target.value
+    });
+  }
+
+  handleMessage(event){
+    this.setState({
+      message: event.target.value
+    });
+  } 
+
+  handleMood(event) {
+    const currentMood = event.currentTarget;
+    if (currentMood.checked) {
+      this.setState({ mood: currentMood.value })
+    }
+  }
+  
+  saveData(){
+    this.setState(prevState => {
+      const newData = [...prevState.data, { newDate : this.state.date, newMood: this.state.mood, newMessage : this.state.message }]
+      return { data: newData };
+    })
+  }
+
   render(){
+    const {data} = this.state;
     return (
       <div className="App">
         <button className="add__button">+</button>
         <ul className="faces__list">
-          <li className="face__item">
-            <div className="smile__face"></div>
-          </li>
-          <li className="face__item">
-            <div className="sad__face"></div>
-          </li>
-          <li className="face__item"></li>
+          {data.map((item, index) => 
+            <li className="face__item" key={index}>{item.currentMood === 'happy' ?
+              <span className="smile__face">{item.currentMood}</span>
+              :
+              <span className="sad__face">{item.currentMood}</span>
+            }
+            </li>
+          )}
         </ul>
 
         <form action="">
           <div className="date__container">
             <label htmlFor="date">Fecha</label>
-            <input type="text" name="date" id="date" placeholder="28/06/2019"/>
+            <input type="text" name="date" id="date" placeholder="28/06/2019" onChange={this.handleDate}/>
           </div>
           <div className="mood__container">
             <div className="happy__mood">
-            <label for="flightoption2">
-              <input
-                id="happy"
-                type="radio"
-                value="happy"
-                name="mood"
-              />
+            <label  htmlFor="mood">
+              <input id="happy" type="radio" value="happy" name="mood" onClick={this.handleMood} />
               :)
             </label>
             </div>
             <div className="sad__mood">
-            <label for="flightoption2">
-              <input
-                id="sad"
-                type="radio"
-                value="sad"
-                name="mood"
-              />
-              :(
-            </label>
+              <label htmlFor="mood">
+                <input id="sad" type="radio" value="sad" name="mood" onClick={this.handleMood} />
+                :(
+              </label>
             </div>
           </div>
-          <div className="message__container">
-            <label htmlFor="happy__message">Mensaje</label>
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="¿Por qué es un buen día?"></textarea>
-          </div>
-          <button className="save__button">Guardar</button>
+          {this.state.mood === "happy" ?
+            <div className="message__container">
+              <label htmlFor="happy__message">Mensaje</label>
+              <textarea name="message" id="message" cols="30" rows="10" placeholder="¿Por qué es un buen día?" onChange={this.handleMessage}></textarea>
+            </div>
+            :
+            null
+          } 
+          
+          <button className="save__button" onClick={this.saveData}>Guardar</button>
           <button className="cancel__button">Cancelar</button>
         </form>
       </div>
